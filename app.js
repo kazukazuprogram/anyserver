a87cd0d35401d527faa859cec969e27ab3e5cda8
proken.vps <proken@www6137ue.sakura.ne.jp>
2020-07-30 16:11:11 +0900

    SERVER ERR

---
+++ app.js
@@ -0,0 +1,19 @@
+// express モジュールのインスタンス作成
+const express = require('express');
+const app = express();
+// パス指定用モジュール
+const path = require('path');
+
+
+// 静的ファイルのルーティング
+app.use(express.static(path.join(__dirname, 'public')));
+
+// その他のリクエストに対する404エラー
+app.use((req, res) => {
+  res.sendStatus(404);
+})
+
+// 3000番ポートで待ちうける
+app.listen(8000, () => {
+  console.log('Running at Port 8000...');
+});
