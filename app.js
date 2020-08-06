148c62a5db1f0f2bd94b9b7939e3cbdb8e71cb71
proken.vps <proken@www6137ue.sakura.ne.jp>
2020-08-06 23:59:13 +0900

    スタイルを調整

--- app.js
+++ app.js
@@ -3,18 +3,35 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 8000;
+const jsonpath = path.join(__dirname, "chat", "chatdata.json");
 
 app.use("/", express.static(path.join(__dirname, 'src')));
 
-// その他のリクエストに対する404エラー
+app.use("/api/chatdata", (req, res)=>{
+  res.header({
+    "Content-Type": "application/json"
+  })
+  textData = fs.readFileSync(jsonpath, "utf-8");
+  res.json(JSON.parse(textData));
+});
+
 app.use((req, res) => {
   res.sendStatus(404);
 })
 
 var http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+function addChatData(data) {
+  rdata = fs.readFileSync(jsonpath, 'utf8')//, function (err, data){
+  obj = JSON.parse(rdata);
+  obj.push({username: data["username"], message:data["message"]});
+  json = JSON.stringify(obj);
+  fs.writeFileSync(jsonpath, json, 'utf8');
+}
 
 io.on('connection', (socket) => {
   console.log("[Websocket] Client connected.")
@@ -23,9 +40,8 @@ io.on('connection', (socket) => {
     if (resMes["type"] == "message") {
       console.log('[WebSocket] Message: ' + msg);
       resMes["type"] = "message_received"
+      addChatData(resMes);
       io.emit('message', JSON.stringify(resMes));
-    } else if (resMes["type"] == "request_pastmessage") {
-      // 過去のメッセージを全て返す
     }
   });
   io.on('disconnect', (socket) => {
--- src/script.js
+++ src/script.js
@@ -1,4 +1,17 @@
 window.onload = () => {
+  window.addMessage = (data) => {
+    console.log("Message received: ");
+    console.log(data);
+    tr = document.createElement("tr");
+    td_name = document.createElement("td");
+    td_name.textContent = data["username"];
+    td_mes = document.createElement("td");
+    td_mes.textContent = data["message"];
+    tr.appendChild(td_name);
+    tr.appendChild(td_mes);
+    mestable.appendChild(tr);
+    meslist.scrollTop = meslist.scrollHeight;
+  }
   console.log("ONLOAD");
   window.socketio = io({
     reconnection: true,
@@ -8,6 +21,7 @@ window.onload = () => {
     randomizationFactor: 0.5
   });
   document.querySelector("div.container").style = "display: none";
+  window.meslist = document.querySelector("div.message_list")
   registForm = document.querySelector("form.register");
   messageForm = document.querySelector("form.message");
   window.mestable = document.querySelector("div.message_list tbody")
@@ -15,19 +29,26 @@ window.onload = () => {
     console.log("Username:"+registForm.username.value)
     window.username = registForm.username.value
     Cookies.set('username', window.username);
+    document.querySelector("div.header div.username").innerHTML = "User: "+username;
     document.querySelector("div.container").style = "";
     document.querySelector("div.welcome_message").style = "display: none;";
+    document.querySelector(".message_text").focus();
     return false;
   }
   if (Cookies.get("username")) {
     window.username = Cookies.get("username");
     console.log("Arleady registered.");
+    document.querySelector("div.header div.username").innerHTML = "User: "+username;
     document.querySelector("div.container").style = "";
     document.querySelector("div.welcome_message").style = "display: none;";
+    document.querySelector("input.message_text").focus();
   }
 
   messageForm.onsubmit = () => {
     message = messageForm.message_text.value
+    if (message.length == 0) {
+      return false
+    }
     send_mes = {
       "type": "message",
       "username": username,
@@ -42,16 +63,23 @@ window.onload = () => {
   socketio.on('message',(msg) => {
     resData = JSON.parse(msg);
     if (resData["type"] == "message_received") {
-      console.log("Message received: ");
-      console.log(resData);
-      tr = document.createElement("tr");
-      td_name = document.createElement("td");
-      td_name.textContent = resData["username"];
-      td_mes = document.createElement("td");
-      td_mes.textContent = resData["message"];
-      tr.appendChild(td_name);
-      tr.appendChild(td_mes);
-      mestable.appendChild(tr);
+      addMessage(resData);
     }
   });
+
+  // 過去のデータを取得、表示
+  fetch('/api/chatdata')
+    .then(response => response.json())
+    .then(json => {
+      for (const data of json) {
+        addMessage(data);
+      }
+    }
+  );
+  document.querySelector("div.header a.logout").onclick = ()=>{
+    Cookies.remove("username");
+    document.querySelector("div.container").style = "display: none;";
+    document.querySelector("div.welcome_message").style = "";
+    document.querySelector("input.username").focus();
+  }
 }
