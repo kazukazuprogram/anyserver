e2c28ff07a31e1db2645921e06a76a1fcf88f87e
kazukazuprogram <33714346+kazukazuprogram@users.noreply.github.com>
2020-08-06 16:27:47 +0900

    Client, .gitignore

---
+++ src/script.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+  console.log("ONLOAD");
+  document.querySelector("div.container").style = "display: none";
+  registForm = document.querySelector("form.register");
+  registForm.onsubmit = () => {
+    console.log("PRESS");
+    console.log(registForm.username.value)
+    window.username = registForm.username.value
+    Cookies.set('username', window.username);
+    document.querySelector("div.container").style = "";
+    document.querySelector("div.welcome_message").style = "display: none;";
+    return false;
+  }
+  if (Cookies.get("username")) {
+    // ユーザー名が既にある
+  }
+  document.querySelector("form.message").onsubmit = () => {
+    console.log("Message send.");
+    return false;
+  }
+}
