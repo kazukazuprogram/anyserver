a385ee5461334bc5107f92a481ade0907e5dece3
proken.vps <proken@www6137ue.sakura.ne.jp>
2020-08-06 18:41:44 +0900

    とりま完成

--- app.js
+++ app.js
@@ -6,24 +6,34 @@ const path = require('path');
 
 const PORT = process.env.PORT || 8000;
 
-// 静的ファイルのルーティング
 app.use("/", express.static(path.join(__dirname, 'src')));
 
 // その他のリクエストに対する404エラー
-// app.use((req, res) => {
-//   res.sendStatus(404);
-// })
+app.use((req, res) => {
+  res.sendStatus(404);
+})
 
-// var http = require('http').Server(app);
-// const io = require('socket.io')(http);
-//
-// io.on('connection', (socket) => {
-//   socket.on('message', (msg) => {
-//     console.log('message: ' + msg);
-//   });
-// });
+var http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-// 3000番ポートで待ちうける
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log("[Websocket] Client connected.")
+  socket.on('message', (msg) => {
+    resMes = JSON.parse(msg)
+    if (resMes["type"] == "message") {
+      console.log('[WebSocket] Message: ' + msg);
+      resMes["type"] = "message_received"
+      io.emit('message', JSON.stringify(resMes));
+    } else if (resMes["type"] == "request_pastmessage") {
+      // 過去のメッセージを全て返す
+    }
+  });
+  io.on('disconnect', (socket) => {
+    console.log("[Websocket] Client disconnected.")
+  });
+});
+
+
+http.listen(PORT, () => {
   console.log('Running at Port ' + PORT + '...');
 });
--- src/script.js
+++ src/script.js
@@ -1,7 +1,16 @@
 window.onload = () => {
   console.log("ONLOAD");
+  window.socketio = io({
+    reconnection: true,
+    reconnectionAttempts: Infinity,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 5000,
+    randomizationFactor: 0.5
+  });
   document.querySelector("div.container").style = "display: none";
   registForm = document.querySelector("form.register");
+  messageForm = document.querySelector("form.message");
+  window.mestable = document.querySelector("div.message_list tbody")
   registForm.onsubmit = () => {
     console.log("Username:"+registForm.username.value)
     window.username = registForm.username.value
@@ -11,10 +20,38 @@ window.onload = () => {
     return false;
   }
   if (Cookies.get("username")) {
-    // ユーザー名が既にある
+    window.username = Cookies.get("username");
+    console.log("Arleady registered.");
+    document.querySelector("div.container").style = "";
+    document.querySelector("div.welcome_message").style = "display: none;";
   }
-  document.querySelector("form.message").onsubmit = () => {
-    console.log("Message send.");
+
+  messageForm.onsubmit = () => {
+    message = messageForm.message_text.value
+    send_mes = {
+      "type": "message",
+      "username": username,
+      "message": message
+    }
+    console.log("Send message: " + message);
+    socketio.emit('message', JSON.stringify(send_mes));
+    messageForm.message_text.value = "";
     return false;
   }
+
+  socketio.on('message',(msg) => {
+    resData = JSON.parse(msg);
+    if (resData["type"] == "message_received") {
+      console.log("Message received: ");
+      console.log(resData);
+      tr = document.createElement("tr");
+      td_name = document.createElement("td");
+      td_name.textContent = resData["username"];
+      td_mes = document.createElement("td");
+      td_mes.textContent = resData["message"];
+      tr.appendChild(td_name);
+      tr.appendChild(td_mes);
+      mestable.appendChild(tr);
+    }
+  });
 }
