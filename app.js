32a951fa2d6ce0dedca1eb663e0624bc8425a307
kazukazuprogram <33714346+kazukazuprogram@users.noreply.github.com>
2020-08-07 17:17:04 +0900

    app.jsを作業前の状態に戻した

--- app.js
+++ app.js
@@ -1,52 +1,19 @@
-#!/usr/bin/env node
-
+// express モジュールのインスタンス作成
 const express = require('express');
 const app = express();
+// パス指定用モジュール
 const path = require('path');
-const fs = require('fs');
 
-const PORT = process.env.PORT || 8000;
-const jsonpath = path.join(__dirname, "chat", "chatdata.json");
 
-app.use("/", express.static(path.join(__dirname, 'src')));
+// 静的ファイルのルーティング
+app.use(express.static(path.join(__dirname, 'src')));
 
-app.use("/api/chatdata", (req, res)=>{
-  res.header({
-    "Content-Type": "application/json"
-  })
-  textData = fs.readFileSync(jsonpath, "utf-8");
-  res.json(JSON.parse(textData));
-});
-
+// その他のリクエストに対する404エラー
 app.use((req, res) => {
   res.sendStatus(404);
 })
 
-var http = require('http').Server(app);
-const io = require('socket.io')(http);
-
-function addChatData(data) {
-  rdata = fs.readFileSync(jsonpath, 'utf8')//, function (err, data){
-  obj = JSON.parse(rdata);
-  obj.push({username: data["username"], message:data["message"]});
-  json = JSON.stringify(obj);
-  fs.writeFileSync(jsonpath, json, 'utf8');
-}
-
-io.on('connection', (socket) => {
-  console.log("[Websocket] Client connected.")
-  socket.on('message', (msg) => {
-    resMes = JSON.parse(msg)
-    if (resMes["type"] == "message") {
-      console.log('[WebSocket] Message: ' + msg);
-      resMes["type"] = "message_received"
-      addChatData(resMes);
-      io.emit('message', JSON.stringify(resMes));
-    }
-  });
-});
-
-
-http.listen(PORT, () => {
-  console.log('Running at Port ' + PORT + '...');
+// 3000番ポートで待ちうける
+app.listen(8000, () => {
+  console.log('Running at Port 8000...');
 });
